58a37eff58ca3f968198c94976d09c5a96819232
code_captain <code_captain@163.com>
2024-12-30 21:34:46 +0800

    [modified] 1.添加user.vue下标提示

--- demo2/pages/user/user.vue
+++ demo2/pages/user/user.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		
+		<view class="user" @click="clickUser">用户信息</view>
 	</view>
 </template>
 
@@ -10,10 +10,28 @@
 			return {
 				
 			};
+		},
+		methods: {
+			clickUser() {
+				uni.removeTabBarBadge({
+					index: 2,
+					text: ""
+				})
+			}
+		},
+		onLoad() {
+			uni.setTabBarBadge({
+				index: 2,
+				text: "3"
+			})
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.user {
+	padding: 20rpx;
+	border: 1rpx solid red;
+	border-radius: 25rpx;
+}
 </style>
